6ff0ea9274b5333f4677061f3109bb5bfa430986
Marcelo Moscoso <marcelo.moscoso@gmail.com>
2018-11-27 00:59:11 -0400

    inicio de proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Alumno } from './models/alumno';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'registro-angular-simple';
+  
+  alumnoArray: Alumno[] = [
+    {id: 1, nombre_completo: 'Marcelo Moscoso', identidad: '4545458'},
+    {id: 2, nombre_completo: 'Ana Gomez', identidad: '7878789'},
+    {id: 3, nombre_completo: 'Jorge Valquez', identidad: '2323238'}
+  ];
+
+  selectedAlumno: Alumno = {id:0, nombre_completo: '', identidad: ''};
+
+  openForEdit(Alumno: Alumno): void{
+    this.selectedAlumno = Alumno;
+  }
+
+  addOrEdit(): void{
+    if(this.selectedAlumno.id === 0) // INSERT
+    {
+      this.selectedAlumno.id = this.alumnoArray.length + 1;
+      this.alumnoArray.push(this.selectedAlumno);
+    }
+    this.selectedAlumno = {id:0, nombre_completo: '', identidad: ''};
+  }
+
+  delete(): void{
+    if(confirm('Are you sure you want to delete it?'))
+    {
+      this.alumnoArray = this.alumnoArray.filter(x => x != this.selectedAlumno);
+      this.selectedAlumno = {id:0, nombre_completo: '', identidad: ''};
+    }
+  }
 }
